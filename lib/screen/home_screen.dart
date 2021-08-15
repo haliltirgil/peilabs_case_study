6081f9a0343d122aaa4075e62e43471d1d0b000f
haliltirgil <tirgil18@gmail.com>
2021-08-15 03:15:17 +0300

    Parse json data and show  screen

--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -1,65 +1,113 @@
-import 'package:flutter/material.dart';
+import 'dart:convert';
 
-class HomePage extends StatelessWidget {
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'package:peilabs_project/model/product.dart';
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text("Ürün Listeleme"),
       ),
-      body: SingleChildScrollView(
-        child: Column(
-          children: [
-            buildRows(context),
-            buildColumns(context),
-          ],
-        ),
+      body: Container(
+        child: _showAllData(),
       ),
     );
   }
 
-  buildRows(BuildContext context) {
-    return Container(
-      child: Row(
-        children: [
-          Container(
-            width: MediaQuery.of(context).size.width / 3,
-            height: 100,
-            color: Colors.pink,
-          ),
-          Container(
-            width: MediaQuery.of(context).size.width / 3,
-            height: 100,
-            color: Colors.white,
-          ),
-          Container(
-            width: MediaQuery.of(context).size.width / 3,
-            height: 100,
-            color: Colors.yellow,
-          ),
-        ],
-      ),
+  _showAllData() {
+    return FutureBuilder(
+      future: fetchProducts(),
+      builder: (BuildContext context, AsyncSnapshot snapshot) {
+        if (snapshot.data == null) {
+          return Container(
+            child: Center(
+              child: Text("Loading...."),
+            ),
+          );
+        } else {
+          return ListView.builder(
+            itemCount: snapshot.data.length,
+            itemBuilder: (context, index) {
+              return ListTile(
+                leading: CircleAvatar(
+                  backgroundImage: NetworkImage(snapshot.data[index].picture),
+                ),
+                title: Text(snapshot.data[index].id.toString()),
+              );
+            },
+          );
+        }
+      },
     );
   }
 
-  buildColumns(BuildContext context) {
-    return Container(
-      child: Column(
-        children: [
-          Container(
-            height: 200,
-            color: Colors.red,
-          ),
-          Container(
-            height: 200,
-            color: Colors.blue,
-          ),
-          Container(
-            height: 200,
-            color: Colors.green,
-          ),
-        ],
-      ),
+  _buildRows(BuildContext context) {
+    return SizedBox(
+      height: MediaQuery.of(context).size.height / 4,
+      child: ListView.builder(
+          physics: ClampingScrollPhysics(),
+          shrinkWrap: true,
+          scrollDirection: Axis.horizontal,
+          itemCount: 4,
+          itemBuilder: (BuildContext context, int index) {
+            return Card(
+              child: Center(
+                child: Text('Kategori Alanı'),
+              ),
+            );
+          }),
     );
+  }
+
+  _buildColumns(BuildContext context) {
+    return ListView.builder(
+      physics: NeverScrollableScrollPhysics(),
+      shrinkWrap: true,
+      itemCount: 8,
+      itemBuilder: (context, index) {
+        return SizedBox(
+          height: MediaQuery.of(context).size.height / 8,
+          child: Card(
+            child: Center(
+              child: Text('Ürünler'),
+            ),
+          ),
+        );
+      },
+    );
+  }
+
+  _buildSizedBox(BuildContext context) {
+    return SizedBox(
+      height: MediaQuery.of(context).size.height / 40,
+    );
+  }
+
+  Future<List<Product>> fetchProducts() async {
+    var url = Uri.parse(
+        "https://raw.githubusercontent.com/haliltirgil/peilabs_jsonfile/main/jsonArray.json");
+    var response = await http.get(url);
+    var jsonData = json.decode(response.body);
+    List<Product> items = [];
+
+    for (var pr in jsonData) {
+      Product p = Product(
+          id: pr["id"],
+          parentId: pr["parentId"],
+          picture: pr["picture"],
+          name: pr["name"]);
+
+      items.add(p);
+    }
+
+    return items;
   }
 }
